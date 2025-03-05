5823af230d63abc9608bd72b58af82d57b7e80e0
RishabhBhairvey <bhairveyrishabh17@gmail.com>
2025-03-05 22:01:07 +0530

    if else

---
+++ normal if condition.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+cout<<"Enter no. pass = ";
+cin>>a;
+if(a>=1000 && a<=9999){
+cout<<"go ahed";
+}
+else{
+cout<<"invalid";
+}
+
+
+
+}
