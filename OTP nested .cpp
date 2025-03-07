e23b1b6d904e59800e90ae5602f6916722d595ed
RishabhBhairvey <bhairveyrishabh17@gmail.com>
2025-03-07 23:40:07 +0530

    if else

---
+++ OTP nested .cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,otp;
+cout<<"Enter your pass = ";
+cin>>a;
+if(a>=1000 && a<=9999){
+if(a==5353){
+cout<<"Enter OTP";
+}
+}
+if(otp==1000){
+    cout<<"otp matched";
+}
+else{cout<<"otp unmatched";
+}
+}
+else{
+cout<<"invalid";
+}
+
+
+
+
+
+}
+
---
+++ if else pass.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+cout<<"Enter your pass = ";
+cin>>a;
+if(a>=1000 && a<=9999){
+if(a==5353){
+cout<<"welcome";
+}
+else{
+cout<<"incorrect pass";
+}
+}
+else{
+cout<<"invalid";
+}
+
+
+
+
+
+}
