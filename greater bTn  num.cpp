fdff5baf47f217e3235218750fede69474958925
RishabhBhairvey <bhairveyrishabh17@gmail.com>
2025-03-07 23:59:29 +0530

    greater value ladder(if else)

---
+++ greater bTn  num.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b,c;
+cout<<"enter 1st number = ";
+cin>>a;
+cout<<"enter 2nd number = ";
+cin>>b;
+cout<<"enter 3rd number = ";
+cin>>c;
+
+if(a>b && a>c){
+cout<<a<<"a is greater";
+}
+else if(b>a && b>c){
+cout<<b<<"b is greater";
+}
+else{
+cout<<c<<"c is greater";
+}
+
+
+
+
+
+
+
+
+}
