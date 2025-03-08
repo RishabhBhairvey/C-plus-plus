fdaff97f6ab7053d0398ec6fb2c5ad8ab872059d
RishabhBhairvey <bhairveyrishabh17@gmail.com>
2025-03-09 01:21:19 +0530

    leap year

---
+++ leap year.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int a;
+cout<<"enter a year = ";
+cin>>a;
+if(a%4==0 && a%100!=0){
+    cout<<a<<"this year is leap year"<<endl;
+}
+else{
+    cout<<a<<"this is not a leap year"<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+}
