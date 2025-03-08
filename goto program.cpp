f17242444a0f0a86626182f9bdb5864a4dfce221
RishabhBhairvey <bhairveyrishabh17@gmail.com>
2025-03-09 00:00:31 +0530

    new file

---
+++ goto program.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+
+cout<<"enter a number \t";
+cin>>a;
+f:
+if(a<=10){
+        cout<<a;
+a++;
+}
+goto f;
+cout<<a;
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ use of goto.cpp
@@ -0,0 +1,20 @@
+// use of goto to jump from one place to another
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"welcome \n";
+goto a;
+cout<<"hello world \n";
+cout<<"i am rishabh \n";
+a:
+cout<<"ok bye \n";
+
+
+
+
+
+
+
+
+
+}
