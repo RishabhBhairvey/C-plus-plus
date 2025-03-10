01d4c85f35707bb6c893930f94b30088f32f9b98
RishabhBhairvey <bhairveyrishabh17@gmail.com>
2025-03-10 23:51:32 +0530

    atm soft

---
+++ atm soft.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b,c,d;
+cout<<"enter pass \n";
+cin>>a;
+
+if(a==1234){
+        cout<<"password access successfull \n";
+}
+cout<<"choose language";
+if(b==1 && b!=1){
+    cout<<"For English press 1";
+
+}
+cout<<" for english  press 1 \t"<<"for hindi press 2 \t" ;
+cin>>b;
+
+cout<<"Hindi\t";
+cin>>c;
+r:
+if(b==1){
+    cout<<"enter amount";
+
+}
+p:
+if(c==2){
+            cout<<"rashi dale";
+        }
+
+else if(d==1000){
+        cout<<"wait for money";
+
+}
+else{
+    cout<<"invalid input";
+}
