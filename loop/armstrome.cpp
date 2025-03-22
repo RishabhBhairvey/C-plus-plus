55f66cc1f5eb4fbb5da913d28326812281f75dde
RishabhBhairvey <bhairveyrishabh17@gmail.com>
2025-03-22 23:45:37 +0530

    armstrome

---
+++ loop/armstrome.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b=0;
+
+cout<<"Enter a armstrome number";
+cin>>a;
+int c=a;
+do{
+        int o=a%10;
+b=b+(o*o*o);
+a=a/10;1
+}while(a>0);
+cout<<b<<"\n";
+if(b==c){
+    cout<<"is a armstrome no.";
+}
+else{
+    cout<<"not a armstrong";
+}
+
+
+
+
+
+
+
+}
---
+++ loop/sum of digit.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b=0,c=0,k;
+cout<<"enter 3 digit no.\n";
+cin>>a;
+k=n;
+if(a<=999 && a>=100){
+do{
+c=a%10;
+b=b+(c*c*c);
+a=a/10;
+
+}while(n>0);
+
+
+}
+
+}
