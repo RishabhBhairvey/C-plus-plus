21abbc8f95e93238e25c3faeb6b678271f415c48
RishabhBhairvey <bhairveyrishabh17@gmail.com>
2025-03-25 16:39:27 +0530

    factorial loop

---
+++ loop/factorial.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,k=1;
+cout<<"enter a no.";
+cin>>a;
+for(a;a>1;a--){
+k=k*a;}
+cout<<k;
+
+
+
+
+
+
+}
---
+++ loop/generate power.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int b,p,j=1,i;
+cout<<"enter a base";
+cin>>b;
+cout<<"enter  power";
+cin>>p;
+
+for(i=1;i<=p;i++){
+j=j*b;
+}
+cout<<j;
+
+
+}
