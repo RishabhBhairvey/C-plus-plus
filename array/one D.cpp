dcede157ddbd18eb8c4b85ce965f0272f4a535f0
RishabhBhairvey <bhairveyrishabh17@gmail.com>
2025-04-01 23:20:35 +0530

    one D arr drawback

---
+++ array/one D.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+//1D arr start from 0 indexing;
+int main(){
+int arr[5]={19,18,13,15,12};
+cout<<arr[0];
+//drawback of one D arr
+//it is hackable from zero indexing to first data ;
+}
